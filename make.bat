2006c502695e636e367d64f6e54a16268ac9015a
sunhailinLeo <379978424@qq.com>
2021-03-26 12:03:46 +0800

    update ci/cd script

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
 cd test
-pytest . --cov=fastapi_apollo_middleware --cov=test && cd .. && flake8 --exclude build --max-line-length 89 --ignore=F401
+pytest . --cov=fastapi_apollo_middleware --cov=test && cd .. && flake8 --exclude build --max-line-length 89 --ignore=F401,F821
